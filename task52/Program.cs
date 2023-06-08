8a6311e38cab56eac2ea1e2837bf857a19690c5f
Olesya Kudr <ala.kud00@mail.ru>
2023-06-08 16:03:24 +0700

    Добавила 52

---
+++ task52/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillMatrixWhithRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} " + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
+
+
+System.Console.WriteLine("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{Math.Round(sum / matrix.GetLength(0), 3)}; ");
+}
